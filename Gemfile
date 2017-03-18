f25472f94ebc1af9a6b22c0d99284171b38405d8
yutoJ <yutoogata4@gmail.com>
2017-03-18 08:13:57 +0000

    first draft

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ gem 'rails_12factor', group: :production
 gem 'devise'
 
 gem 'faker'
+
+gem 'rails_admin'
+
+gem 'cancan'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
