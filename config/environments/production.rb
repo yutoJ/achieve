d8bfa88b3f97d4216cc82f463e6c6679a0e49dfa
yutoJ <yutoogata4@gmail.com>
2017-03-18 04:59:38 +0000

    BUGFIX: typo stmp to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['APP_DOMAIN_NAME'] }
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.stmp_settings =
+  ActionMailer::Base.smtp_settings =
   {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
