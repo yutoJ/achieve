77a648ecad54fc77f10f7c9907a0a5ae1a2acd1f
yutoJ <yutoogata4@gmail.com>
2017-03-20 05:45:01 +0000

    cancan comment out

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
-    cannot :manage, Contact
+    #cannot :manage, Contact
     #cannot :access, :new_contact
 
     if user && user.admin?
