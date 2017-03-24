843844fc16a29ab93cf1c09396a0c9b3f7bdd413
yutoJ <yutoogata4@gmail.com>
2017-03-24 17:15:38 +0000

    poem ver her

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,4 +1,6 @@
-class Poem < ActiveResource::Base #ActiveRecord::Baseから変更した
+#class Poem < ActiveResource::Base #ActiveRecord::Baseから変更した
+class Poem# < ActiveRecord::Base
   #self.site = "http://" + "#{ENV['APP_DOMAIN_NAME']}"
-  self.site = "https://stormy-lake-92712.herokuapp.com/"
+  #self.site = "https://stormy-lake-92712.herokuapp.com/"
+  include Her::Model
 end
