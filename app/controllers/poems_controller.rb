0c9baf2bf8621012eb8398b9cfae183bc1d643ac
yutoJ <yutoogata4@gmail.com>
2017-03-25 08:24:09 +0000

    herでformat .json でhttpを送る実装を追加、これでApiSampleはjsonを返してくれる

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,11 +1,11 @@
 class PoemsController < ApplicationController
 
   def index
-    @poems = Poem.all
+    @poems = Poem.all(format: 'json')
   end
 
   def show
     #@poem = params
-    @poem = Poem.find(params[:id])
+    @poem = Poem.find(params[:id], format: 'json')
   end
 end
