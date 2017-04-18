dde52e1abf2deddd6d109913c5f5e3a912ac7717
yuto <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-04-18 17:17:13 +0000

    test blog model validation with Rspec

---
+++ spec/models/blogs_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Blog do
+  it "is valid with title" do
+    blog = Blog.new(title: "test by Rspec", content: "I am a tester")
+    expect(blog).to be_valid
+  end
+
+  it "is invalid without a title" do
+    blog = Blog.new
+    expect(blog).not_to be_valid
+  end
+
+  it "is valid with title" do
+    blog = Blog.new
+    blog.valid?
+    expect(blog.errors[:title]).to include("を入力してください")
+  end
+end
