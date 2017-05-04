eda8e888c98a1ff9a422398bb8bba43c1e7a2d88
yuto <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-05-04 10:37:03 +0000

    fix seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-10.times do |n|
+1.times do |n|
     email = Faker::Internet.email
     name = Faker::LordOfTheRings.character
     password = ENV['SEED_USER_PASSWORD']
